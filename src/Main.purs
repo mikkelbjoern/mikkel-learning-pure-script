d1e542f5035fbd87ef444087077b28f4ed3149c7
Mikkel B. Goldschmidt <m@goldsch.dk>
2018-02-03 21:05:28 +0100

    Solved Euler 1

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,17 @@
 module Main where
 
 import Control.Monad.Eff.Console
-import Math (sqrt)
-import Prelude
 
-diagonal w h = sqrt (w * w  + h * h)
+import Prelude (mod, (+), (-), (==))
+import Data.HeytingAlgebra((||))
 
-main = logShow (diagonal 4.0 3.0)
+
+f:: Int -> Int -> Int
+f x 0 = x
+f x y =
+  if ((y `mod` 3) == 0) || ((y `mod` 5) == 0)
+    then f (x+y) (y-1)
+    else f x (y-1)
+
+
+main = logShow (f 0 999)
